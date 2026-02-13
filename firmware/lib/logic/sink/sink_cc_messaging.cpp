e7c60d797b1cd2bc35bf93abbde42833ae2dab92
Marco Tabini <marcot@tabini.ca>
2026-02-13 09:46:41 -0500

    Place guards around how messages are enqueued.

--- firmware/lib/logic/sink/sink_cc_messaging.cpp
+++ firmware/lib/logic/sink/sink_cc_messaging.cpp
@@ -57,8 +57,23 @@ void Sink::_onMessageReceived(const T76::DRPD::PHY::BMCDecodedMessage *message) 
     _runtimeState._lastReceivedMessageId = receivedMessageId;
     _bmcEncoder.sendGoodCRCForDecodedMessage(*message);
 
+    if (_messageQueue == nullptr) {
+        return;
+    }
+
     const T76::DRPD::PHY::BMCDecodedMessage* messagePtr = message;
-    xQueueSendToBack(_messageQueue, &messagePtr, portMAX_DELAY);
+
+#if defined(portCHECK_IF_IN_ISR)
+    if (portCHECK_IF_IN_ISR()) {
+        BaseType_t higherPriorityTaskWoken = pdFALSE;
+        (void)xQueueSendToBackFromISR(_messageQueue, &messagePtr, &higherPriorityTaskWoken);
+        portYIELD_FROM_ISR(higherPriorityTaskWoken);
+        return;
+    }
+#endif
+
+    // Never block the decoder callback path; drop if the queue is full.
+    (void)xQueueSendToBack(_messageQueue, &messagePtr, 0);
 }
 
 Sink::ExtendedFragmentResult Sink::_handleExtendedMessageFragment(
