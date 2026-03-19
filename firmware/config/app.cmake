65d0c5fc417094cfa606c87dafc8bf4bbc9d532f
Marco Tabini <marcot@tabini.ca>
2026-03-19 09:12:18 -0400

    feat: increase message queue and buffer sizes for improved data handling in BMC decoder

--- firmware/config/app.cmake
+++ firmware/config/app.cmake
@@ -3,6 +3,5 @@
 # Compile-time definitions for the App
 
 target_compile_definitions(drpd-firmware PUBLIC
-    APP_RECEIVED_MESSAGE_QUEUE_LENGTH=30          # Length of the queue for received messages from PHY layer
-    APP_CORE1_TIMER_FREQUENCY_HZ=10000            # Frequency of the timer used for core 1 tasks in Hz
+    APP_RECEIVED_MESSAGE_QUEUE_LENGTH=250          # Length of the queue for received messages from PHY layer
 )
