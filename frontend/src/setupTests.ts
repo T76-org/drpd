9aa3870da6ac52e6d202c2dfd0fb70a273dbf95f
Marco Tabini <marcot@tabini.ca>
2026-06-04 09:02:57 -0400

    feat: Add mock CanvasRenderingContext2D for testing purposes

--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -1,1 +1,25 @@
 import '@testing-library/jest-dom'
+
+const buildCanvasContext = (): CanvasRenderingContext2D => ({
+  canvas: document.createElement('canvas'),
+  clearRect: () => undefined,
+  drawImage: () => undefined,
+  fillRect: () => undefined,
+  fillText: () => undefined,
+  measureText: (text: string) => ({ width: text.length * 6 }) as TextMetrics,
+  restore: () => undefined,
+  save: () => undefined,
+  scale: () => undefined,
+  setTransform: () => undefined,
+  stroke: () => undefined,
+  strokeRect: () => undefined,
+  strokeText: () => undefined,
+  translate: () => undefined,
+}) as unknown as CanvasRenderingContext2D
+
+Object.defineProperty(HTMLCanvasElement.prototype, 'getContext', {
+  configurable: true,
+  value(contextId: string) {
+    return contextId === '2d' ? buildCanvasContext() : null
+  },
+})
